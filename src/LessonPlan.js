6ad58cc3f0255b167089db394859f7e0b759a928
rafaelsinger <rafaelsinger66@gmail.com>
2023-09-17 00:06:50 -0400

    fixing errors with useNavigate()

--- src/LessonPlan.js
+++ src/LessonPlan.js
@@ -1,30 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, {useEffect} from 'react';
+
+import { useLocation } from "react-router-dom";
 
 const LessonPlan = () => {
-  const [lessonPlan, setLessonPlan] = useState(''); // State to store the lesson plan text
-  const [error, setError] = useState(null); // State to store any errors
+  const [lessonPlan, setLessonPlan] = React.useState();
+  const [error, setError] = React.useState();
+
+  const getLessonPlan = () => {
+    const location = useLocation();
+    setLessonPlan(location.state.lessonPlan);
+  };
 
   useEffect(() => {
-    // Fetch the lesson plan from the server when the component mounts
-    const fetchLessonPlan = async () => {
-      try {
-        // Make an Axios POST request to your server
-        const response = await axios.post('http://localhost:3001/gpt-response', {
-          userAssistant: ["Generate a lesson plan for AP Mathematics about Integration for 12th graders."] // Or whatever prompt you want to send
-        });
-
-        // Update the `lessonPlan` state with the received data
-        setLessonPlan(response.data.messageContent);
-
-      } catch (error) {
-        // Handle any errors
-        setError(error.message);
-      }
-    };
-
-    fetchLessonPlan();
-  }, []); // Empty dependency array means this useEffect runs once when the component mounts
+    getLessonPlan();
+  }, []);
 
   return (
     <div className="lesson-plan-container">
